9c9caa57a360c242f0daee72c455f4c5eb551a8c
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2026-01-29 19:19:09 -0300

    Sales validation API created

---
+++ shop-validator/src/test/java/br/dev/paulovictorsilva/shop_validator/ShopValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.paulovictorsilva.shop_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
